d95db0b8a777dcae8340bb8748668257b4974b2d
song-gyubin <song_gyubin@kakaku.com>
2021-06-01 12:20:02 +0900

    login view completed

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,14 +6,17 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // Override point for customization after application launch.
+        FirebaseApp.configure()    // 追加
         return true
     }
 
